57e01d8b7928176b7a4299309ee85a8db79e0133
Amir Ali <verifiedloser.buner@gmail.com>
2023-05-11 13:06:14 +0500

    DB Fixes

--- server.js
+++ server.js
@@ -2,30 +2,34 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const app = require('./app');
 
-mongoose
-  .connect(process.env.DATABASE_URI, {
+const run = async () => {
+  await mongoose.connect(process.env.DATABASE_URI, {
     dbName: 'Natours',
-  })
-  .then((_con) => console.log('db connected'))
-  .catch((error) => console.log(error.message));
-
+  });
+};
 const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () =>
-  console.log(`App Running on http://localhost:${PORT}`)
-);
 
-process.on('uncaughtException', (err) => {
-  console.log('uncaught Exeception! 💥 Shuttig Down!');
-  server.close(() => {
-    process.exit(1);
-  });
-});
-process.on('unhandledRejection', (err) => {
-  console.log('Unhandled Rejection! 💥 Shuttig Down!');
-  console.log(err);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+run()
+  .then((_con) => {
+    console.log('db connected');
+    const server = app.listen(PORT, () =>
+      console.log(`App Running on http://localhost:${PORT}`)
+    );
+
+    process.on('uncaughtException', (err) => {
+      console.log('uncaught Exeception! 💥 Shuttig Down!');
+      server.close(() => {
+        process.exit(1);
+      });
+    });
+    process.on('unhandledRejection', (err) => {
+      console.log('Unhandled Rejection! 💥 Shuttig Down!');
+      console.log(err);
+      server.close(() => {
+        process.exit(1);
+      });
+    });
+  })
+  .catch((error) => console.log(error.message));
 
 // process.on('uncaughtException');
